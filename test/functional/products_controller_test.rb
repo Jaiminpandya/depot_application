792fc6fbac8efcbffa6372934984f02c6b37f877
Jaimin <jaiminpandya@example.com>
2014-06-17 17:41:50 +0530

    Finish layout and routes

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -3,11 +3,10 @@ require 'test_helper'
 class ProductsControllerTest < ActionController::TestCase
    setup do
      @product = products(:one)
-     @update = {
-       title:       'Lorem Ipsum',
-       description: 'Wibbles are fun!',
-       image_url:   'lorem.jpg',
-       price:       19.95
+     @update = {title:       'Lorem Ipsum',
+                description: 'Wibbles are fun!',
+                image_url:   'lorem.jpg',
+                price:       19.95
 }
 end
 
